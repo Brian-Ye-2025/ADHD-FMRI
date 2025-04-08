dd31103768e88f92f38368e7ce28d27b41ff19ed
Brian-Ye-2025 <brianye28@gmail.com>
2025-04-08 10:05:02 -0500

    Update masked_glass_plot.py

--- functions/masked_glass_plot.py
+++ functions/masked_glass_plot.py
@@ -1,1 +1,14 @@
+from nilearn.masking import compute_brain_mask
+from nilearn.image import math_img
 
+def masked_glass_plot(file_path, volume_index=0):
+    #Converts 4D to 3D
+    volume_img = index_img(file_path, volume_index)
+
+    #Applies masking
+    adhd_brain_mask = compute_brain_mask(volume_img)
+    adhd_masked_img = math_img("img * mask", img=extracted1, mask=adhd_brain_mask)
+
+    #Generates glass plot
+    plot_glass_brain(adhd_masked_img, display_mode='ortho')
+    plt.show()
