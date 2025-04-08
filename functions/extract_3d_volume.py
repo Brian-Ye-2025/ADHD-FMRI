f1aa52c4733622a893b78f17614f8c316664510a
Brian-Ye-2025 <brianye28@gmail.com>
2025-04-08 10:02:58 -0500

    Update extract_3d_volume.py

--- functions/extract_3d_volume.py
+++ functions/extract_3d_volume.py
@@ -1,1 +1,6 @@
+from nilearn.image import index_img
 
+def extract_3d_volume(fmri_img_path, volume_index=0):
+    #Extracts a moment from the 4th axis as marked by volume_index
+    volume_img = index_img(fmri_img_path, volume_index)
+    return volume_img
